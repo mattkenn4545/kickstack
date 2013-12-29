740ea4634cebf15308927b18c634d03be94f2bdc
Matthew Kennedy <mattkenn4545@gmail.com>
2013-12-29 05:34:46 -0700

    Remove line

--- manifests/role/network.pp
+++ manifests/role/network.pp
@@ -2,6 +2,5 @@ class kickstack::role::network inherits kickstack {
   include kickstack::neutron::agent::dhcp
   include kickstack::neutron::agent::l3
   include kickstack::neutron::agent::l2
-
   include kickstack::neutron::agent::metadata
 }
