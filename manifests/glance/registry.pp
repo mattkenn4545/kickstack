8edf0450752f1bd1fd0d10915e9484b0d177e137
Matthew Kennedy <mattkenn4545@gmail.com>
2013-12-30 00:47:53 -0700

    Remove missing fact elseif that should not have been there

--- manifests/glance/registry.pp
+++ manifests/glance/registry.pp
@@ -1,8 +1,6 @@
 class kickstack::glance::registry inherits kickstack::glance {
   if (!$db_host) {
     $missing_fact = 'db_host'
-  } elsif (!$glance_registry_host) {
-    $missing_fact = 'glance_registry_host'
   } elsif (!$auth_host) {
     $missing_fact = 'auth_host'
   }
