149a5b17e5adad62954da71388b5a1209349906e
Matthew Kennedy <mkennedy@clearwateranalytics.com>
2014-01-03 16:28:09 -0700

    Move comment over (missed earlier)

--- manifests/horizon.pp
+++ manifests/horizon.pp
@@ -1,5 +1,11 @@
 class kickstack::horizon (
   $secret_key           = hiera('kickstack::horizon::secret_key',           'horizon_secret_key'),
+
+  # Allow access to Horizon using any host name?
+  # Default is false, meaning allow Horizon access only through the
+  # FQDN of the dashboard host.
+  # Set to true if you want to access by IP address, through an SSH
+  # tunnel, etc.
   $allow_any_hostname   = hiera('kickstack::horizon::allow_any_hostname',   false)
 ) inherits kickstack {
   validate_bool($allow_any_hostname)
