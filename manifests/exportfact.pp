d48e067ea8ae851346ca96172ba3b1dd9f9ca95d
Matthew Kennedy <mattkenn4545@gmail.com>
2014-01-25 00:54:41 -0700

    Missed $ and small cleanup

--- manifests/exportfact.pp
+++ manifests/exportfact.pp
@@ -3,10 +3,10 @@ define kickstack::exportfact {
 
   $value = getvar("kickstack::params::${name}")
 
-  if ($value == $fqdn or value == getvar("ipaddress_${kickstack::params::nic_management}") or !$value) {
+  if ($value == $fqdn or $value == getvar("ipaddress_${kickstack::params::nic_management}") or !$value) {
     ::exportfact::export { "${kickstack::params::kickstack_environment}_${name}":
       value     => $name ? { 'nova_metadata_ip' => getvar("ipaddress_${kickstack::params::nic_management}"),
-                             default => $fqdn },
+                             default            => $fqdn },
       category  => $kickstack::params::kickstack_environment
     }
   }
